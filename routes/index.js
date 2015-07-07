42b52f64913925c6462c4c1f306de220085a87b2
Aidan Garnish <garnish@outlook.com>
2015-07-07 20:19:09 +0100

    About page added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express App' });
 });
 
+/* GET about page */
+router.get('/about', function(req, res, next){
+  res.render('about', { title: 'About'})
+})
 module.exports = router;
